3c49abe20999ba6edad9b8aafcde45d95fe03892
Akanksha-Encircle <akankshap.encircle@gmail.com>
2024-12-19 19:25:42 +0530

    banner, snack, journey

--- js/script.js
+++ js/script.js
@@ -26,13 +26,59 @@
             $("html").removeClass("overflow-hidden");
         });
 
-		var swiper = new Swiper(".bannerSwiper", {
+		if ($(".header").length > 0) {
+			var stickyTop = $(".header").offset().top + 200;
+
+			$(window).scroll(function () {
+				var windowTop = $(window).scrollTop();
+				if (
+					stickyTop < windowTop &&
+					$(".content").height() +
+					$(".content").offset().top -
+					$(".header").height() >
+					windowTop) {
+					$(".header").addClass("atfix");
+				} else {
+					$('.header').removeClass("atfix");
+				}
+			});
+		}
+
+		var swiperBanner = new Swiper(".bannerSwiper", {
 			slidesPerView: 1,
 			spaceBetween: 30,
 			loop: true,
 			navigation: {
 				nextEl: ".banner-swiper-button-next",
 				prevEl: ".banner-swiper-button-prev",
+			},
+		});
+
+		var swiperSnack = new Swiper(".snackSwiper", {
+			slidesPerView: 1,
+			spaceBetween: 20,
+			loop: true,
+			navigation: {
+				nextEl: ".snack-swiper-button-next",
+				prevEl: ".snack-swiper-button-prev",
+			},
+			breakpoints: {
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 40,
+				},
+				1024: {
+				  slidesPerView: 2,
+				  spaceBetween: 80,
+				},
+				1280: {
+				  slidesPerView: 3,
+				  spaceBetween: 40,
+				},
+				1432: {
+				  slidesPerView: 3,
+				  spaceBetween: 80,
+				},
 			},
 		});
 
